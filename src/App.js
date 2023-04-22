be0767f11eee1369e0386e12dc353e3d0f373a81
sarthak kaul <sarthakkaul68@gmail.com>
2023-04-23 03:01:58 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Temp from './components/weather/temp';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Temp/>
+    </div>
+  );
+}
+
+export default App;
